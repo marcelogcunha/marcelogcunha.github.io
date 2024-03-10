af49dcc61d9e404de58ee33db82ccb44c9f033e0
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-03-10 22:25:26 +0000

    Update livechat.js

--- mobile/livechat.js
+++ mobile/livechat.js
@@ -2,7 +2,7 @@
 function initEmbeddedMessaging() {
 		try {
 			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-			
+			embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
 
 			embeddedservice_bootstrap.init(
 				'00D3N000000HTnt',
@@ -12,6 +12,7 @@ function initEmbeddedMessaging() {
 					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
 				}
 			);
+			embeddedservice_bootstrap.utilAPI.launchChat();
 		} catch (err) {
 			console.error('Error loading Embedded Messaging: ', err);
 		}
