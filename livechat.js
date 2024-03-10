90d01f1d3561d84dbdabc167e2927e05ee216937
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-03-10 21:03:44 +0000

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,19 +1,36 @@
+chatLoaded = false;
+window.addEventListener("onEmbeddedMessagingReady", () => {
+    console.log("Received the onEmbeddedMessagingReady event…");
+  
+    this.chatLoaded = true;
+  });
+
 function initEmbeddedMessaging() {
-        try {
-			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+    try {
+      embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+      embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
 
-			embeddedservice_bootstrap.init(
-				'00D3N000000HTnt',
-				'Valeo_Live_Chat',
-				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChat1708964206329',
-				{
-					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
-				}
-			);
-		} catch (err) {
-			console.error('Errorr loading Embedded Messaging: ', err);
-		}
+      embeddedservice_bootstrap.init(
+        '00D3N000000HTnt',
+        'Valeo_Live_Channel_External',
+        'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChannelExt1710018844438',
+        {
+          scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
+        }
+      );
+    } catch (err) {
+      console.error('Error loading Embedded Messaging: ', err);
+    }
 
-}
+    
+    
+  };
 
-initEmbeddedMessaging();
+  function launchChat() {
+    embeddedservice_bootstrap.utilAPI.launchChat();
+  }
+
+  initEmbeddedMessaging();
+    
+
+
