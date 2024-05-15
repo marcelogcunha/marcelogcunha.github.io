8824ffde3de34f6607096d917d200d70a3a658f4
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 17:29:00 +0100

    Update livechat.js

--- mobile/livechat.js
+++ mobile/livechat.js
@@ -2,6 +2,7 @@ function initEmbeddedMessaging() {
 		try {
 			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
 			embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
+			embeddedservice_bootstrap.utilAPI.launchChat();
 
 			embeddedservice_bootstrap.init(
 				'00D3N000000HTnt',
