070ec4742e97bc5898aa7c0247ceb5ec919ba447
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 18:16:17 +0100

    Update livechat.js

--- mobile/livechat.js
+++ mobile/livechat.js
@@ -1,3 +1,22 @@
+
+function initEmbeddedMessaging() {
+		try {
+			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+			embeddedservice_bootstrap.init(
+				'00D3N000000HTnt',
+				'Valeo_Live_Chat_External_Github_Pages_mobile',
+				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChatExtern1710106536106',
+				{
+					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
+				}
+			);
+		} catch (err) {
+			console.error('Error loading Embedded Messaging: ', err);
+		}
+	};
+initEmbeddedMessaging();
+
+
 window.addEventListener("onEmbeddedMessagingReady", () => {
   console.log("Received the onEmbeddedMessagingReady event…");
 	embeddedservice_bootstrap.utilAPI.launchChat()
@@ -20,21 +39,3 @@ window.addEventListener('message', (event) => {
         }, event.origin);
     }
 });
-function initEmbeddedMessaging() {
-		try {
-			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-			embeddedservice_bootstrap.init(
-				'00D3N000000HTnt',
-				'Valeo_Live_Chat_External_Github_Pages_mobile',
-				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChatExtern1710106536106',
-				{
-					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
-				}
-			);
-		} catch (err) {
-			console.error('Error loading Embedded Messaging: ', err);
-		}
-	};
-initEmbeddedMessaging();
-
-
