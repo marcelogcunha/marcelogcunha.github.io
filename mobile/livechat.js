5279dabe5a62871dd0c29006af0e6312bde0cfd9
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 18:00:02 +0100

    Update livechat.js

--- mobile/livechat.js
+++ mobile/livechat.js
@@ -1,9 +1,6 @@
 function initEmbeddedMessaging() {
 		try {
 			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-			
-			
-
 			embeddedservice_bootstrap.init(
 				'00D3N000000HTnt',
 				'Valeo_Live_Chat_External_Github_Pages_mobile',
@@ -12,33 +9,27 @@ function initEmbeddedMessaging() {
 					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
 				}
 			);
-			
 		} catch (err) {
 			console.error('Error loading Embedded Messaging: ', err);
 		}
-			
 	};
- function launchChat() {
-    embeddedservice_bootstrap.utilAPI.launchChat();
-  }
 initEmbeddedMessaging();
+
 window.addEventListener("onEmbeddedMessagingReady", () => {
   console.log("Received the onEmbeddedMessagingReady event…");
 	embeddedservice_bootstrap.utilAPI.launchChat()
 		.then(() => {
 			embeddedservice_bootstrap.utilAPI.hideChatButton();
 		});
-
   // The JavaScrip API is ready for calls
 });
+
 // Listen for messages from the iframe
 window.addEventListener('message', (event) => {
     // Check the message sent by the iframe
     if (event.data === 'getURLParams') {
-	    console.log("gitlog - event.origin = ", event.origin);
         // Get the URL parameters from the parent window's URL
         const params = new URLSearchParams(window.location.search);
-        
         // Respond to the iframe with the URL parameters
         event.source.postMessage({
             type: 'URLParams',
