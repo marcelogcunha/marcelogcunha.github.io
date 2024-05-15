25b6cb0ee9cc85a6d494623e0b9aca1ff2775cbe
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 23:59:29 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -5,6 +5,7 @@ document.body.appendChild(script);
 function initEmbeddedMessaging() {
     try {
 	embeddedservice_bootstrap = window.embeddedservice_bootstrap || {}; // Ensure embeddedservice_bootstrap is defined globally
+	    embeddedservice_bootstrap.settings = embeddedservice_bootstrap.settings || {};
       embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
       embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
       embeddedservice_bootstrap.init(
