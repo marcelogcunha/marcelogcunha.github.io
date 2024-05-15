9692a5b4b8b3910a8f6818d381b10cb78d5a6106
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 18:21:23 +0100

    Update livechat.js

--- mobile/livechat.js
+++ mobile/livechat.js
@@ -2,7 +2,6 @@
 function initEmbeddedMessaging() {
 		try {
 			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-			embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
 			embeddedservice_bootstrap.init(
 				'00D3N000000HTnt',
 				'Valeo_Live_Chat_External_Github_Pages_mobile',
