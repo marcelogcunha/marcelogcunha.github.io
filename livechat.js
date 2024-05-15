a9f8b45d8a4bff57ce440373728436cf2e36fa4d
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-15 15:49:05 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,24 +1,11 @@
-
-window.addEventListener("onEmbeddedMessagingReady", () => {
-    embeddedservice_bootstrap.prechatAPI.setVisiblePrechatFields({
-    // List the pre-chat field names with the value and whether
-    // it's editable in the pre-chat form.
-    "_firstName": {
-      "value": "Jane",
-      "isEditableByEndUser": false
-    }
-  });
-  });
-
 function initEmbeddedMessaging() {
     try {
       embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
       embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
-
       embeddedservice_bootstrap.init(
 				'00D3N000000HTnt',
-				'Valeo_Live_Chat',
-				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChat1708964206329',
+				'Valeo_Live_Chat_External_Github_Pages_mobile',
+				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChatExtern1710106536106',
 				{
 					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
 				}
@@ -26,31 +13,22 @@ function initEmbeddedMessaging() {
     } catch (err) {
       console.error('Error loading Embedded Messaging: ', err);
     }
-
-    
-    
   };
 
-  function launchChat() {
-    embeddedservice_bootstrap.utilAPI.launchChat();
-  }
+function launchChat() {
+	embeddedservice_bootstrap.utilAPI.launchChat();
+}
 
-  initEmbeddedMessaging();
+initEmbeddedMessaging();
+
+//window.addEventListener("onEmbeddedMessagingReady", () => {});
 
 // Listen for messages from the iframe
 window.addEventListener('message', (event) => {
-    // Check the origin of the message to ensure it's coming from a trusted source
-    if (event.origin !== 'https://valeocare4u--sccp2dev.sandbox.my.site.com') {
-	    console.log('github console.log - event.origin = ', event.origin);
-        return;
-    }
-
     // Check the message sent by the iframe
     if (event.data === 'getURLParams') {
-	    console.log("gitlog - event.origin = ", event.origin);
         // Get the URL parameters from the parent window's URL
         const params = new URLSearchParams(window.location.search);
-        
         // Respond to the iframe with the URL parameters
         event.source.postMessage({
             type: 'URLParams',
