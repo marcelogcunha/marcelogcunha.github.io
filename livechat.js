65968f022ed7bedb2ea03ea80f9cd6c1714dabd9
Marcelo Cunha <marcelogcunha7@gmail.com>
2024-05-20 18:51:54 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,29 +1,28 @@
 function initEmbeddedMessaging() {
     try {
-      embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-      embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
-      embeddedservice_bootstrap.init(
-				'00D3N000000HTnt',
-				'Valeo_Live_Chat_External_Github_Pages_mobile',
-				'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChatExtern1710106536106',
-				{
-					scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
-				}
-			);
+        embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+        embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
+        embeddedservice_bootstrap.init(
+            '00D3N000000HTnt',
+            'Valeo_Live_Chat_External_Github_Pages_mobile',
+            'https://valeocare4u--sccp2dev.sandbox.my.site.com/ESWValeoLiveChatExtern1710106536106', {
+                scrt2URL: 'https://valeocare4u--sccp2dev.sandbox.my.salesforce-scrt.com'
+            }
+        );
     } catch (err) {
-      console.error('Error loading Embedded Messaging: ', err);
+        console.error('Error loading Embedded Messaging: ', err);
     }
-  };
+};
 
 function launchChat() {
     embeddedservice_bootstrap.utilAPI.launchChat()
-        .then(function (success) {
+        .then(function(success) {
             // Add actions to run after the chat client launches successfully.
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log(error);
         })
-        .finally(function () {
+        .finally(function() {
             // Add actions to run whether the chat client launches
             // successfully or not.
         });
@@ -42,6 +41,3 @@ window.addEventListener('message', (event) => {
         }, event.origin);
     }
 });
-    
-
-
